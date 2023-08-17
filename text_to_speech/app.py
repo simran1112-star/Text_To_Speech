ec92ea23be69d1601944ccb3aa873933086cb5f8
simran arora <140587380+simran1112-star@users.noreply.github.com>
2023-08-17 14:15:16 +0530

    Updated app.py

--- text_to_speech/app.py
+++ text_to_speech/app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template, request
-import requests
 from bs4 import BeautifulSoup
 import boto3
 import os
-import sys
-
+import requests
+from langdetect import detect
+from indic_transliteration import sanscript
+from indic_transliteration.sanscript import transliterate
 app = Flask(__name__)
+
+
+def convert_to_devanagari(text, source_script):
+    devanagari_text = transliterate(text, source_script, sanscript.DEVANAGARI)
+    return devanagari_text
+
+def convert_gurmukhi_to_devanagari(text):
+    return convert_to_devanagari(text, 'gurmukhi')
+
+def convert_gujarati_to_devanagari(text):
+    return convert_to_devanagari(text, 'gujarati')
+
+def convert_bengali_to_devanagari(text):
+    return convert_to_devanagari(text, 'bengali')
 
 def aws_polly_text_to_speech(text):
     aws_mag_con = boto3.session.Session(profile_name='test_user')
@@ -13,7 +28,7 @@ def aws_polly_text_to_speech(text):
 
     chunk_size = 2000
     i = 0
-    audio_chunks = []  # Store audio chunks filenames
+    audio_chunks = []  
 
     while i < len(text):
         chunk = text[i:i + chunk_size]
@@ -36,21 +51,21 @@ def aws_polly_text_to_speech(text):
 
         i += chunk_size
 
-    # Combine audio chunks into a single audio file
-    combined_audio = b"".join([open(filename, "rb").read() for filename in audio_chunks])
+    combined_audio = b""
+    for filename in audio_chunks:
+        with open(filename, "rb") as f:
+            audio_data = f.read()
+            combined_audio += audio_data
 
-    # Save the combined audio to a file
     with open("output_combined.mp3", "wb") as f:
-        print("rdtftgh")
         f.write(combined_audio)
 
-    # Clean up individual audio chunk files
     for filename in audio_chunks:
         os.remove(filename)
 
 @app.route('/')
 def home():
-    return render_template('try.html')
+    return render_template('index.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -65,16 +80,39 @@ def submit():
             soup = BeautifulSoup(html_content, 'html.parser')
             extracted_text = soup.get_text()
 
-            aws_polly_text_to_speech(extracted_text)
-            return f"<h2>Conversion Result</h2><p>Text extracted from URL: {extracted_text}</p>"
+            detected_lang = detect(extracted_text)
+
+            if detected_lang == 'bn':  
+                devanagari_text = convert_bengali_to_devanagari(extracted_text)
+            elif detected_lang == 'pa':  
+                devanagari_text = convert_gurmukhi_to_devanagari(extracted_text)
+            elif detected_lang == 'gu':  
+                devanagari_text = convert_gujarati_to_devanagari(extracted_text)
+            else:
+                devanagari_text = extracted_text
+            
+            aws_polly_text_to_speech(devanagari_text)
+            return f"<h2>Conversion Result</h2><p>Text extracted from URL: {devanagari_text}</p>"
 
         except requests.exceptions.RequestException as e:
             return f"<h2>Error</h2><p>Failed to fetch URL: {e}</p>"
 
-    else:
+    elif input_option == 'text':  
         text_input = request.form.get('textInput')
-        aws_polly_text_to_speech(text_input)
-        return f"<h2>Conversion Result</h2><p>Text input: {text_input}</p>"
+
+        detected_lang = detect(text_input)
+
+        if detected_lang == 'bn':  
+            devanagari_text = convert_bengali_to_devanagari(text_input)
+        elif detected_lang == 'pa':  
+            devanagari_text = convert_gurmukhi_to_devanagari(text_input)
+        elif detected_lang == 'gu': 
+            devanagari_text = convert_gujarati_to_devanagari(text_input)
+        else:
+            devanagari_text = text_input
+
+        aws_polly_text_to_speech(devanagari_text)
+        return f"<h2>Conversion Result</h2><p>Text input: {devanagari_text}</p>"
 
 if __name__ == '__main__':
     app.run(debug=True)
